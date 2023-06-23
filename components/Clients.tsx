a3419f2ca04858b3d29868e2623c58f97684be06
peculiarnewbie <curiousarmand@gmail.com>
2023-06-23 23:15:06 +0700

    fix clients section

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -6,13 +6,15 @@ export default function Clients({notion} : {notion:any}){
     console.log(notion)
 
     return(
-        <div className="Container" style={{marginTop: '50px'}}>
+        <section className="Container" style={{marginTop: '50px'}}>
 
             <div className='ClientsArea'>
                 {notion.map((item:any, index:number) => (
-                    <img key={index} src={item}></img>
+                    <div className='ClientsItem'>
+                        <img key={index} src={item}></img>
+                    </div>
                     ))}
             </div>
-        </div>
+        </section>
     )
 }
