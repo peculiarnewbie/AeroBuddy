7258e48e2069d563b1b06e6c5cd7078c1820c9ee
peculiarnewbie <curiousarmand@gmail.com>
2023-06-27 18:36:33 +0700

    navbar for apps

--- app/applications/secondapp/page.tsx
+++ app/applications/secondapp/page.tsx
@@ -12,7 +12,7 @@ export default async function(){
         <Header session={session}></Header>
         {(session ?
             <div style={{display: 'flex',}}>
-                <iframe style={{width: '100vw', height:'100vh'}} src="https://lookerstudio.google.com/embed/u/1/reporting/54e2fcc2-06e6-421a-8a66-c00471e8f260/page/p_hbuco6fc7c"></iframe> 
+                <iframe style={{width: '100vw', height:'100vh'}} src=""></iframe> 
             </div>
         : <div style={{
             display: 'flex',
