adf3bb7a0293221124f7ea4016e501b61fc49614
peculiarnewbie <curiousarmand@gmail.com>
2023-06-29 13:44:48 +0700

    implement activity log

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,14 +21,82 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   console.log('session:', session);
 
+  const logActivity = async (pathname:string) => {
+    'use server';
+
+    if(!session){
+        return;
+    }
+
+    let page = ''
+    if(pathname == '/'){
+        page = "Home"
+    }
+    else if(pathname == '/applications/lookerstudio'){
+        page = "Looker Studio"
+    }
+    else if(pathname == '/applications/prompt'){
+        page = "Prompt"
+    }
+
+    const body = JSON.stringify(
+        {
+          "parent": {
+              "database_id": "a7c1ff2dabaf4128b3e857f1276959c9"
+          },
+          "properties":{
+              "Name":{
+                  "title":[{
+                      "text":{
+                          "content": session?.user?.name
+                      }
+                  }]
+              },
+              "Email":{
+                  "email" : session?.user?.email 
+              },
+              "Page":{
+                  "select":{
+                      "name": page
+                  }
+              }
+          }
+      }
+    )
+
+    console.log(body)
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Notion-Version': '2022-06-28',
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${process.env.NOTION_KEY}`
+      },
+      cache: 'no-cache',
+      body: body
+    };
+
+    //@ts-ignore
+    const raw = await fetch(`https://api.notion.com/v1/pages`, options)
+
+    const response = await raw.json()
+
+    console.log('logged', response);
+
+    return response;
+  }
+
   return (
     <html lang="en">
       {/* <Splash></Splash> */}
       <body className={`${inter.className}`}>
         <LoadingSplash></LoadingSplash>
-        <Header session={session}></Header>
+        <Header session={session} logFunction={logActivity}></Header>
         {children}
       </body>
     </html>
   )
 }
+
+
